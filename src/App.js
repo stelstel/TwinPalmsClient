be0a2aca99f0ffa3cfd084fad605ea48195a3457
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-05-21 10:41:58 +0200

    Added DataReports route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Navbar from './components/navigation/Navbar';
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Login from './components/authentication/Login';
 import MultiStepForm from './components/report/MultiStepForm';
 import Home from './components/home/Home';
-import Carousel from './components/analytics/Carousel';
+//import Carousel from './components/analytics/Carousel';
+import DataReports from './components/DataReports/DataReports';
 import CreateUser from './components/createuser/CreateUser';
 import Dashboard from './components/admin/Dashboard';
 
@@ -14,13 +15,13 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path='/' exact component={Home}/>
-          <Route path='/home' component={Home}/>
-          <Route path='/login' component={Login}/>
-          <Route path='/report' component={MultiStepForm}/>
-          <Route path='/datareports' component={Carousel}/>
-          <Route path='/admin' component={Dashboard}/>
-          <Route path='/createuser' component={CreateUser}/>
+          <Route path="/" exact component={Home} />
+          <Route path="/home" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/report" component={MultiStepForm} />
+          <Route path="/datareports" component={DataReports} />
+          <Route path="/admin" component={Dashboard} />
+          <Route path="/createuser" component={CreateUser} />
         </Switch>
       </Router>
     </>
