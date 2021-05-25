1d90eb057a54d981916765a04a39ba014fddb15d
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-05-25 09:56:06 +0200

    DataReports Charts added

---
+++ src/components/DataReports/Charts/index.js
@@ -0,0 +1,6 @@
+import Revenue3D from './Revenue3D';
+//import Bar3D from './Bar3D';
+import GuestPie3D from './GuestPie3D';
+//import Doughnut2D from './Doughnut2d';
+
+export { Revenue3D, GuestPie3D };
