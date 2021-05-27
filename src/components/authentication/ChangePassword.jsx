9ed46a518ec4f868709ab04f3df34c27e35857d4
Henrik <web@villprogrammera.nu>
2021-05-27 15:27:15 +0200

    change password functionality

---
+++ src/components/authentication/ChangePassword.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+/* import { Link } from "react-router-dom"; */
+import { Grid, Paper, Avatar, TextField, Button } from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import "./Login.css";
+
+function ChangePassword(props) {
+  const avatarStyle = { backgroundColor: "#1bbd7e", marginTop: "30px" };
+
+  let [state, setState] = useState({
+    currentPassword: "",
+    newPassword: "",
+  });
+
+  function handleChange(e) {
+    const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: value,
+    });
+  }
+
+  return (
+    <Grid className="login-page-container">
+      <Grid style={{ paddingTop: "60px" }}>
+        <Paper className="login-paper" elevation={10}>
+          <Grid align="center">
+            <Avatar style={avatarStyle}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <h2 style={{ marginTop: 30 }}>Change password</h2>
+          </Grid>
+          <form onSubmit={props.onSubmitFunction}>
+            <TextField
+              onChange={handleChange}
+              id="currentPassword"
+              label="Current Password"
+              placeholder="Enter password"
+              style={{ marginTop: "40px" }}
+              fullWidth
+              required
+            />
+            <TextField
+              onChange={handleChange}
+              id="newPassword"
+              label="New Password"
+              placeholder="Enter password"
+              style={{ marginTop: "40px" }}
+              type="password"
+              fullWidth
+              required
+            />
+
+            <Button
+              type="submit"
+              color="primary"
+              variant="contained"
+              style={{ marginTop: "60px" }}
+              fullWidth
+            >
+              Change password
+            </Button>
+          </form>
+        </Paper>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default ChangePassword;
