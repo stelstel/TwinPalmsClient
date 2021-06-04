7092e6784b7f01b997c12c8cd21a093ae98e9738
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-06-04 10:30:22 +0200

    Holds the data reports utils/misc

---
+++ src/components/DataReports/utils/misc.js
@@ -0,0 +1,12 @@
+// Server Routes
+//export const API_URL = 'https://localhost:44306/api/FbReports/';
+// Gets list of all Outlets
+export const API_OUTLET_URL = 'https://localhost:44306/api/Outlets';
+//Gets all FbReports for many outlets between fromDate and toDate, including toDate
+export const API_URL =
+  'https://localhost:44306/outlets/fbReports?outletIds=1&fromDate=2021-04-02&toDate=2021-06-02';
+
+/*
+export const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${}&query=`;
+export const POPULAR_BASE_URL = `${API_URL}movie/popular?api_key=${}`;
+*/
