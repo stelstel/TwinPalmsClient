c9dbd6b4476a057fd4976892197a5a4a967d9f3b
Sheliann Olsson <sheliannohlsson@yahoo.com>
2021-06-14 10:28:40 +0200

    Data Reports Chart

---
+++ src/components/DataReports/Chart/Chart.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import LineChart from './LineChart';
+
+const Chart = () => {
+  // const [chartData, setChartData] = useState([]);
+
+  return (
+    <div>
+      <LineChart />
+    </div>
+  );
+};
+export default Chart;
