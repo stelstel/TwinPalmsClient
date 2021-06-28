6b586f738a03542f7e58dbac983be4ccff70933c
Robin Granberg <robin.granberg@hotmail.com>
2021-06-28 11:03:28 +0200

    Added random css changes and remade most of datareports page

--- src/components/DataReports/DataReports.jsx
+++ src/components/DataReports/DataReports.jsx
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 // Component
-import Table from "../DataReports/Table/TableData";
 import DatePicker from "../DataReports/DatePicker/DatePicker";
 import Chart from "../DataReports/Chart/Chart";
 import { UserContext } from "../../App";
@@ -11,26 +10,23 @@ import "./DataReports.css";
 export default function DataReports( ) {
   const user = useContext(UserContext);
 
-  // Date time
-  const currentDate = new Date();
-  const date = currentDate.toDateString();
-
   return (
     <>
       <section className="table-section">
         <br />
-        <h1 className="table-title">Data Reports Summary</h1>
+        <div className="table-title">
+          <h1>DATA REPORTS SUMMARY</h1>
+        </div>
         <br />
 
         <div id="Maindiv">
           <h4 className="statistic-date">
-            Todays Statistics: {date}
             <DatePicker user={user} />
             <br />
             <br />
           </h4>
           <br />
-          <Table />
+          
           <hr />
           <Chart />
         </div>
--- src/components/DataReports/utils/misc.js
+++ src/components/DataReports/utils/misc.js
@@ -4,7 +4,7 @@
 export const API_OUTLET_URL = 'https://localhost:44306/api/Outlets';
 //Gets all FbReports for many outlets between fromDate and toDate, including toDate
 export const API_URL =
-  "http://localhost/outlets/fbReports?outletIds=1&fromDate=2021-01-01&toDate=2021-06-04";
+  "https://localhost:44306/outlets/fbReports?outletIds=1&fromDate=2021-01-01&toDate=2021-06-27";
 
 /*
 export const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${}&query=`;
